30176c37e0e11a08e0a42cc6341a3fe530a8d53b
Serhii Ovchinnikov <50240729+ovchinnikovs@users.noreply.github.com>
2019-09-16 00:34:51 +0300

    Updates

---
+++ precache-manifest.1db47d4a3c4c721d8eedb4ca7a979dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ce322f6ff79f9df77f464bd5a3ecad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fcdf18a6d74945f2e07",
+    "url": "/static/css/main.b705578e.chunk.css"
+  },
+  {
+    "revision": "33e721c164459fac25aa",
+    "url": "/static/js/2.2c8d4c24.chunk.js"
+  },
+  {
+    "revision": "2fcdf18a6d74945f2e07",
+    "url": "/static/js/main.27ce4f37.chunk.js"
+  },
+  {
+    "revision": "71196f50aafdf2de0968",
+    "url": "/static/js/runtime~main.40b65e00.js"
+  },
+  {
+    "revision": "70c66a1defb2fca6b034c549b96cd64e",
+    "url": "/static/media/photo.70c66a1d.jpg"
+  }
+]);
